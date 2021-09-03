9170511c89d09bd07f9bbc24d898578987a289e2
Jon Smith <aardvarkjon94@yahoo.com>
2021-09-02 23:16:04 -0500

    Finished basic funtionality with initial list

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,37 @@
+characters = [
+    {
+        name: "Achilles",
+        type: "Warrior"
+    },
+    {
+        name: "Agni",
+        type: "Mage"
+    },
+    {
+        name: "Ah Muzen Cab",
+        type: "Hunter"
+    },
+    {
+        name: "Ah Puch",
+        type: "Mage"
+    },
+    {
+        name: "Amaterasu",
+        type: "Warrior"
+    },
+    {
+        name: "Ah Puch",
+        type: "Mage"
+    }
+]
 const picker = document.getElementById("picker")
 const container = document.getElementById("container")
+const p = document.createElement('p')
 
 picker.addEventListener("click", (e) => {
-    const p = document.createElement('p')
-    p.innerHTML = "clicked"
+    const random = Math.floor(Math.random() * characters.length)
+    
+    p.innerHTML = characters[random].name
+    container.innerHTML = ""
     container.appendChild(p)
 })
